f3a0619c0356b119801b0241f8df4b434f6ca7de
Chris <chrisrkemp@hotmail.co.uk>
2020-03-08 12:35:53 +0000

    Added way to id types

--- files/Types/include/id.h
+++ files/Types/include/id.h
@@ -29,4 +29,29 @@ namespace SWIFT
 
 	template<typename UNIQUE_TYPE, typename ID_TYPE>
 	ID_TYPE ID<UNIQUE_TYPE, ID_TYPE>::s_previous_id = 0;
+
+	//Provides a way to ID types
+	//Each different type that we access TYPE_ID with will cause a new id to be created (during static initialization) which is unique to that type
+
+	using TYPE_ID = ID<int>;
+
+	template<typename TYPE>
+	struct GET_TYPE_ID final
+	{
+		const static ID<TYPE_ID> id;
+
+	private:
+		GET_TYPE_ID();	//Private to prevent construction
+	};
+
+	template<typename TYPE>
+	const ID<TYPE_ID> GET_TYPE_ID<TYPE>::id;
+
+	//Check if two types are the same
+
+	template<typename T1, typename T2>
+	bool same_type()
+	{
+		return GET_TYPE_ID<T1>::id == GET_TYPE_ID<T2>::id;
+	}
 }
