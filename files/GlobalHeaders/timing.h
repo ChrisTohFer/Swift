48e1a891d8a6b51313e41894511a3a2daa0d8c1a
Chris <chrisrkemp@hotmail.co.uk>
2020-06-28 13:03:39 +0100

    Added CYCLE_TIMER, arial font and added render time to window

---
+++ files/GlobalHeaders/timing.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <chrono>
+
+namespace SWIFT
+{
+    class CYCLE_TIMER
+    {
+        using TIME_POINT = std::chrono::system_clock::time_point;
+
+    public:
+        CYCLE_TIMER(int samples)
+            : m_durations_size(samples)
+            , m_index(0)
+        {
+            m_durations = new int[samples]();
+            m_previous_time = std::chrono::system_clock::now();
+        }
+        ~CYCLE_TIMER()
+        {
+            delete[] m_durations;
+        }
+
+        void cycle()
+        {
+            auto time = std::chrono::system_clock::now();
+            m_index = (m_index + 1) % m_durations_size;
+            m_durations[m_index] = int(std::chrono::duration_cast<std::chrono::microseconds>(time - m_previous_time).count());
+            m_previous_time = time;
+        }
+
+        int average_cycle_time()
+        {
+            int total = 0;
+            for (auto i = 0; i < m_durations_size; ++i)
+                total += m_durations[i];
+
+            return total / m_durations_size;
+        }
+
+    private:
+        int* m_durations;
+        int  m_durations_size;
+        int  m_index;
+
+        TIME_POINT m_previous_time;
+    };
+}
--- files/Multimedia/src/renderer.cpp
+++ files/Multimedia/src/renderer.cpp
@@ -1,6 +1,8 @@
 #include "renderer.h"
 #include "internal_types.h"
 
+#include "GlobalHeaders/timing.h"
+
 #include "SFML/Graphics.hpp"
 
 #include <thread>
@@ -35,22 +37,39 @@ SWIFT::RENDERER::IMPL::~IMPL()
 
 #include <cmath>
 #include <chrono>
+#include "SFML/Graphics/Font.hpp"
 void SWIFT::RENDERER::IMPL::render_loop()
 {
+	//temporary
+	sf::Font font;
+	font.loadFromFile("../../resources/fonts/arial.ttf");
+	sf::Text cycle_time_text;
 	sf::CircleShape circle;
+	cycle_time_text.setFont(font);
+	cycle_time_text.setFillColor(sf::Color::White);
+	cycle_time_text.setCharacterSize(20);
 	circle.setFillColor(sf::Color::Green);
 	circle.setRadius(50.f);
-
 	auto start_time = std::chrono::system_clock::now();
+	//~temporary
+
+	CYCLE_TIMER timer(100);
 	while (m_running)
 	{
+		timer.cycle();
+
+		//temporary
+		auto temp = std::to_string(timer.average_cycle_time());
+		cycle_time_text.setString(temp);
 		auto now = std::chrono::system_clock::now();
 		auto time = now - start_time;
 		auto time_millis = std::chrono::duration_cast<std::chrono::milliseconds>(time);
 		circle.setRadius(50.f * abs(std::cos(time_millis.count() * 0.001f)));
+		//~temporary
 
 		m_window.clear(sf::Color::Black);
 		m_window.draw(circle);
+		m_window.draw(cycle_time_text);
 		m_window.display();
 
 		using namespace std::chrono_literals;
