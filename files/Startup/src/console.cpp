05a1511797c60ccff28d85249c7b212b8a635551
Chris <chrisrkemp@hotmail.co.uk>
2020-07-11 18:11:11 +0100

    Reorganised console to be human readable

--- files/Startup/src/console.cpp
+++ files/Startup/src/console.cpp
@@ -12,7 +12,7 @@ SWIFT::CONSOLE::CONSOLE(std::wistream& istream, std::wostream& ostream)
 SWIFT::CONSOLE::~CONSOLE()
 {
     m_running = false;
-    //m_thread.join(); We don't join here as the thread may be waiting for cin, just allow the thread to terminate forcefully
+    //m_thread.join(); We don't join here as the thread may be waiting for cin, just allow the thread to terminate forcefully on destruction
 }
 
 void SWIFT::CONSOLE::remove_console_command(std::wstring command)
@@ -41,9 +41,10 @@ void SWIFT::CONSOLE::invoke_commands()
 
 void SWIFT::CONSOLE::output(std::wstring const& msg)
 {
-    m_mutex.lock();
+    static std::mutex output_mutex; //There is no need to use the same mutex for the output/input sides of the console
+    output_mutex.lock();
     m_ostream << msg;
-    m_mutex.unlock();
+    output_mutex.unlock();
 }
 
 void SWIFT::CONSOLE::console_loop()
@@ -53,21 +54,15 @@ void SWIFT::CONSOLE::console_loop()
     while (m_running)
     {
         check_input();
-
-        using namespace std::chrono_literals;
-        std::this_thread::sleep_for(1ms);
     }
 }
 
 void SWIFT::CONSOLE::check_input()
 {
-    m_istream.peek();   //I don't know why, but nothing can be entered to the console if we dont do this
-
     std::wstring command_line;
-    std::getline(m_istream, command_line);
+    std::getline(m_istream, command_line);  //We block here while waiting for input - make sure we haven't locked the mutex at this point
 
     std::wistringstream command_stream(command_line);
-
     std::wstring command;
     command_stream >> command;
 
@@ -89,8 +84,9 @@ void SWIFT::CONSOLE::check_input()
 
 void SWIFT::CONSOLE::output_command_map()
 {
+    m_ostream << "\n";
     for (auto& command : m_command_definitions)
     {
-        m_ostream << std::left << std::setw(28) << command.first << "- " << command.second->description << "\n";
+        m_ostream << std::left << std::setw(20) << command.first << " - " << command.second->description << "\n";
     }
 }
--- files/Startup/src/console.h
+++ files/Startup/src/console.h
@@ -13,76 +13,22 @@ namespace SWIFT
 
     class CONSOLE
     {
-        // Command definitions //
-
-        struct COMMAND_DEFINITION
-        {
-            virtual ~COMMAND_DEFINITION() = default;
-            virtual bool register_command(CONSOLE&, std::wistream&) = 0;
-
-            std::wstring description;
-        };
-
+        //Command definitions represent functions which we've added to the console that the user can call via a string
+        struct COMMAND_DEFINITION;
         template<typename ...ARGS>
-        struct COMMAND_DEFINITION_NONMEMBER : public COMMAND_DEFINITION
-        {
-            COMMAND_DEFINITION_NONMEMBER(std::wstring description, void(*func)(ARGS...));
-            bool register_command(CONSOLE&, std::wistream&) override;
-
-        private:
-            void(*fn)(ARGS...);                 //Function pointer
-        };
-
+        struct COMMAND_DEFINITION_NONMEMBER;
         template<typename T, typename ...ARGS>
-        struct COMMAND_DEFINITION_MEMBER : public COMMAND_DEFINITION
-        {
-            COMMAND_DEFINITION_MEMBER(std::wstring description, T* target, void(T::*func)(ARGS...));
-            bool register_command(CONSOLE&, std::wistream&) override;
+        struct COMMAND_DEFINITION_MEMBER;
 
-        private:
-            T* m_target;
-            void(T::*fn)(ARGS...);                 //Function pointer
-        };
-
-        // Commands //
-
-        struct COMMAND
-        {
-            virtual ~COMMAND() = default;
-            virtual void invoke() = 0;
-        };
-
+        //Commands represent requests that the user has made which need to be invoked by the main thread
+        struct COMMAND;
         template<typename ...ARGS>
-        struct COMMAND_NONMEMBER : public COMMAND
-        {
-            COMMAND_NONMEMBER(void(*func)(ARGS...));
-
-            void invoke() override;
-            template<int INDEX, int COUNT>
-            bool read_arguments_from_stream_recursive(std::wistream&);
-
-            void(*fn)(ARGS...);                 //Function pointer
-            std::tuple<ARGS...> arguments;
-        };
-
+        struct COMMAND_NONMEMBER;
         template<typename T, typename ...ARGS>
-        struct COMMAND_MEMBER : public COMMAND
-        {
-            COMMAND_MEMBER(T* target, void(T::*func)(ARGS...));
-
-            void invoke() override;
-            template<int INDEX, int COUNT>
-            bool read_arguments_from_stream_recursive(std::wistream&);
-
-        private:
-            T* m_target;
-            void(T::*fn)(ARGS...);                 //Function pointer
-            std::tuple<ARGS...> arguments;
-        };
+        struct COMMAND_MEMBER;
 
         using COMMAND_DEFINITION_MAP = std::map<std::wstring, std::unique_ptr<COMMAND_DEFINITION>>;
         using COMMANDS = std::vector<std::unique_ptr<COMMAND>>;
-        using OUTPUTS = std::vector<std::wstring>;
 
     public:
         //Functions
@@ -104,14 +50,87 @@ namespace SWIFT
         void check_input();
         void output_command_map();
 
-        std::thread             m_thread;
-        std::mutex              m_mutex;
-        bool                    m_running = true;
-        COMMAND_DEFINITION_MAP  m_command_definitions;
-        COMMANDS                m_commands;
+        std::thread              m_thread;
+        std::mutex               m_mutex;
+        bool                     m_running = true;
+        COMMAND_DEFINITION_MAP   m_command_definitions;
+        COMMANDS                 m_commands;
         std::wistream&           m_istream;
         std::wostream&           m_ostream;
     };
+
+    // Command definitions //
+
+    //Base class for storing pointers
+    struct CONSOLE::COMMAND_DEFINITION
+    {
+        virtual ~COMMAND_DEFINITION() = default;
+        virtual bool register_command(CONSOLE&, std::wistream&) = 0;
+
+        std::wstring description;
+    };
+
+    //Implementation for non member functions
+    template<typename ...ARGS>
+    struct CONSOLE::COMMAND_DEFINITION_NONMEMBER : public COMMAND_DEFINITION
+    {
+        COMMAND_DEFINITION_NONMEMBER(std::wstring description, void(*func)(ARGS...));
+        bool register_command(CONSOLE&, std::wistream&) override;
+
+    private:
+        void(*m_fn)(ARGS...);                 //Function pointer
+    };
+
+    //Implementation for member functions
+    //Assumes that the target will continue to exist and not be moved in memory from the point that it is added to the console
+    template<typename T, typename ...ARGS>
+    struct CONSOLE::COMMAND_DEFINITION_MEMBER : public COMMAND_DEFINITION
+    {
+        COMMAND_DEFINITION_MEMBER(std::wstring description, T* target, void(T::* func)(ARGS...));
+        bool register_command(CONSOLE&, std::wistream&) override;
+
+    private:
+        T* m_target;
+        void(T::* m_fn)(ARGS...);                 //Function pointer
+    };
+
+    // Commands //
+
+    struct CONSOLE::COMMAND
+    {
+        virtual ~COMMAND() = default;
+        virtual void invoke() = 0;
+    };
+
+    template<typename ...ARGS>
+    struct CONSOLE::COMMAND_NONMEMBER : public COMMAND
+    {
+        COMMAND_NONMEMBER(void(*func)(ARGS...));
+
+        void invoke() override;
+        template<int INDEX, int COUNT>
+        bool read_arguments_from_stream_recursive(std::wistream&);
+
+        void(*m_fn)(ARGS...);                 //Function pointer
+        std::tuple<ARGS...> m_arguments;
+    };
+
+    template<typename T, typename ...ARGS>
+    struct CONSOLE::COMMAND_MEMBER : public COMMAND
+    {
+        COMMAND_MEMBER(T* target, void(T::* func)(ARGS...));
+
+        void invoke() override;
+        template<int INDEX, int COUNT>
+        bool read_arguments_from_stream_recursive(std::wistream&);
+
+    private:
+        T* m_target;
+        void(T::* m_fn)(ARGS...);                 //Function pointer
+        std::tuple<ARGS...> m_arguments;
+    };
+
+    //Functions
 
     // CONSOLE //
     
@@ -161,13 +180,13 @@ namespace SWIFT
     inline CONSOLE::COMMAND_DEFINITION_NONMEMBER<ARGS...>::COMMAND_DEFINITION_NONMEMBER(std::wstring description, void(*func)(ARGS...))
     {
         this->description = description;
-        fn = func;
+        m_fn = func;
     }
 
     template<typename ...ARGS>
     inline bool CONSOLE::COMMAND_DEFINITION_NONMEMBER<ARGS...>::register_command(CONSOLE& console, [[maybe_unused]] std::wistream& command_stream)
     {
-        auto command = std::make_unique<COMMAND_NONMEMBER<ARGS...>>(fn);
+        auto command = std::make_unique<COMMAND_NONMEMBER<ARGS...>>(m_fn);
         if constexpr (sizeof...(ARGS) != 0)
         {
             if (!command->read_arguments_from_stream_recursive<0, sizeof...(ARGS)>(command_stream))
@@ -185,13 +204,13 @@ namespace SWIFT
     {
         this->description = description;
         m_target = target;
-        fn = func;
+        m_fn = func;
     }
 
     template<typename T, typename ...ARGS>
     inline bool CONSOLE::COMMAND_DEFINITION_MEMBER<T, ARGS...>::register_command(CONSOLE& console, [[maybe_unused]] std::wistream& command_stream)
     {
-        auto command = std::make_unique<COMMAND_MEMBER<T, ARGS...>>(m_target, fn);
+        auto command = std::make_unique<COMMAND_MEMBER<T, ARGS...>>(m_target, m_fn);
         if constexpr (sizeof...(ARGS) != 0)
         {
             if (!command->read_arguments_from_stream_recursive<0, sizeof...(ARGS)>(command_stream))
@@ -206,20 +225,20 @@ namespace SWIFT
 
     template<typename ...ARGS>
     inline CONSOLE::COMMAND_NONMEMBER<ARGS...>::COMMAND_NONMEMBER(void(*func)(ARGS...))
-        : fn(func)
+        : m_fn(func)
     {}
 
     template<typename ...ARGS>
     inline void CONSOLE::COMMAND_NONMEMBER<ARGS...>::invoke()
     {
-        std::apply(fn, arguments);
+        std::apply(m_fn, m_arguments);
     }
 
     template<typename ...ARGS>
     template<int INDEX, int COUNT>
     inline bool CONSOLE::COMMAND_NONMEMBER<ARGS...>::read_arguments_from_stream_recursive(std::wistream& command_stream)
     {
-        command_stream >> std::get<INDEX>(arguments);
+        command_stream >> std::get<INDEX>(m_arguments);
 
         if (command_stream.fail())
         {
@@ -239,7 +258,7 @@ namespace SWIFT
     template<typename T, typename ...ARGS>
     inline CONSOLE::COMMAND_MEMBER<T, ARGS...>::COMMAND_MEMBER(T* target, void(T::* func)(ARGS...))
         : m_target(target)
-        , fn(func)
+        , m_fn(func)
     {}
 
     template<typename T, typename ...ARGS>
@@ -247,16 +266,16 @@ namespace SWIFT
     {
         auto lambda = [&](ARGS... args)
         {
-            (m_target->*fn)(args...);
+            (m_target->*m_fn)(args...);
         };
-        std::apply(lambda, arguments);
+        std::apply(lambda, m_arguments);
     }
 
     template<typename T, typename ...ARGS>
     template<int INDEX, int COUNT>
     inline bool CONSOLE::COMMAND_MEMBER<T, ARGS...>::read_arguments_from_stream_recursive(std::wistream& command_stream)
     {
-        command_stream >> std::get<INDEX>(arguments);
+        command_stream >> std::get<INDEX>(m_arguments);
 
         if (command_stream.fail())
         {
